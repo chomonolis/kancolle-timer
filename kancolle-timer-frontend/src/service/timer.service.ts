915229afbc0ab58f8fd4a3ea7cd83268c1d1d6e5
chomonolis <tyomo3317@gmail.com>
2022-12-14 17:06:53 +0900

    listtimerのserviceを追加

--- kancolle-timer-frontend/src/service/timer.service.ts
+++ kancolle-timer-frontend/src/service/timer.service.ts
@@ -1,19 +1,23 @@
 import { API } from 'aws-amplify';
 import { GraphQLResult, graphqlOperation } from '@aws-amplify/api-graphql';
 
+import { listTimers } from '../graphql/queries';
 import { createTimer } from '../graphql/mutations';
-import { CreateTimerInput, CreateTimerMutation } from '../API';
+import { CreateTimerInput, CreateTimerMutation, ListTimersQuery } from '../API';
 import { PromiseType } from '../utils/typeUtils';
 
 const TimerService = {
   createTimer: async (input: CreateTimerInput) => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     return API.graphql(graphqlOperation(createTimer, { input: input })) as Promise<GraphQLResult<CreateTimerMutation>>;
+  },
+  listTimers: async () => {
+    return API.graphql(graphqlOperation(listTimers)) as Promise<GraphQLResult<ListTimersQuery>>;
   },
 };
 
 export type TimerServiceReturnType = {
   createTimerRT: PromiseType<ReturnType<typeof TimerService.createTimer>>;
+  listTimersRT: PromiseType<ReturnType<typeof TimerService.listTimers>>;
 };
 
 export default TimerService;
