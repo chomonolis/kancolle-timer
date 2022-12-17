76e353f5b1848dbbaa8ed88f2db48d8a953c735b
chomonolis <tyomo3317@gmail.com>
2022-12-17 17:31:10 +0900

    #8 Timerの更新、削除のサービス及びフック実装

--- kancolle-timer-frontend/src/hook/timer.hook.ts
+++ kancolle-timer-frontend/src/hook/timer.hook.ts
@@ -1,6 +1,6 @@
 import { useCallback } from 'react';
 
-import { CreateTimerInput } from '../API';
+import { CreateTimerInput, UpdateTimerInput } from '../API';
 import TimerService, { TimerServiceReturnType } from '../service/timer.service';
 
 const useTimer = () => {
@@ -8,6 +8,26 @@ const useTimer = () => {
     try {
       const res = await TimerService.createTimer(createTimerInput);
       return res.data?.createTimer;
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  }, []);
+
+  const updateTimer = useCallback(async (updateTimerInput: UpdateTimerInput) => {
+    try {
+      const res = await TimerService.updateTimer(updateTimerInput);
+      return res.data?.updateTimer;
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  }, []);
+
+  const deleteTimer = useCallback(async (id: string) => {
+    try {
+      const res = await TimerService.deleteTimer({ id });
+      return res.data?.deleteTimer;
     } catch (e) {
       console.error(e);
       return null;
@@ -24,11 +44,13 @@ const useTimer = () => {
     }
   }, []);
 
-  return { createTimer, listTimers };
+  return { createTimer, updateTimer, deleteTimer, listTimers };
 };
 
 export type UseTimerReturnType = {
   createTimerRT: Exclude<TimerServiceReturnType['createTimerRT']['data'], undefined>['createTimer'];
+  updateTimerRT: Exclude<TimerServiceReturnType['updateTimerRT']['data'], undefined>['updateTimer'];
+  deleteTimerRT: Exclude<TimerServiceReturnType['deleteTimerRT']['data'], undefined>['deleteTimer'];
   listTimersRT: Exclude<
     Exclude<TimerServiceReturnType['listTimersRT']['data'], undefined>['listTimers'],
     undefined | null
--- kancolle-timer-frontend/src/service/timer.service.ts
+++ kancolle-timer-frontend/src/service/timer.service.ts
@@ -2,13 +2,27 @@ import { API } from 'aws-amplify';
 import { GraphQLResult, graphqlOperation } from '@aws-amplify/api-graphql';
 
 import { listTimers } from '../graphql/queries';
-import { createTimer } from '../graphql/mutations';
-import { CreateTimerInput, CreateTimerMutation, ListTimersQuery } from '../API';
+import { createTimer, updateTimer, deleteTimer } from '../graphql/mutations';
+import {
+  CreateTimerInput,
+  UpdateTimerInput,
+  DeleteTimerInput,
+  CreateTimerMutation,
+  UpdateTimerMutation,
+  DeleteTimerMutation,
+  ListTimersQuery,
+} from '../API';
 import { PromiseType } from '../utils/typeUtils';
 
 const TimerService = {
   createTimer: async (input: CreateTimerInput) => {
     return API.graphql(graphqlOperation(createTimer, { input: input })) as Promise<GraphQLResult<CreateTimerMutation>>;
+  },
+  updateTimer: async (input: UpdateTimerInput) => {
+    return API.graphql(graphqlOperation(updateTimer, { input: input })) as Promise<GraphQLResult<UpdateTimerMutation>>;
+  },
+  deleteTimer: async (input: DeleteTimerInput) => {
+    return API.graphql(graphqlOperation(deleteTimer, { input: input })) as Promise<GraphQLResult<DeleteTimerMutation>>;
   },
   listTimers: async () => {
     return API.graphql(graphqlOperation(listTimers)) as Promise<GraphQLResult<ListTimersQuery>>;
@@ -17,6 +31,8 @@ const TimerService = {
 
 export type TimerServiceReturnType = {
   createTimerRT: PromiseType<ReturnType<typeof TimerService.createTimer>>;
+  updateTimerRT: PromiseType<ReturnType<typeof TimerService.updateTimer>>;
+  deleteTimerRT: PromiseType<ReturnType<typeof TimerService.deleteTimer>>;
   listTimersRT: PromiseType<ReturnType<typeof TimerService.listTimers>>;
 };
 
