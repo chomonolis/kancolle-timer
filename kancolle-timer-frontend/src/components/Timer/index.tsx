102f52951eb49680c01ec6d405ca3300d0676bf9
chomonolis <tyomo3317@gmail.com>
2022-12-23 16:07:43 +0900

    幅を修正

--- kancolle-timer-frontend/src/components/Timer/index.tsx
+++ kancolle-timer-frontend/src/components/Timer/index.tsx
@@ -1,3 +1,4 @@
+import { Box } from '@mui/material';
 import EditTimer from './EditTimer';
 import ShowTimer from './ShowTimer';
 import useTimerIndex from './useIndexTimer';
@@ -6,11 +7,13 @@ const Timer = () => {
   const { timers } = useTimerIndex();
   return (
     <>
-      {timers &&
-        timers.map((t) => {
-          return <ShowTimer key={t.id} timer={t} />;
-        })}
-      <EditTimer timerListSize={timers.length} />
+      <Box sx={{ minWidth: '450px' }}>
+        {timers &&
+          timers.map((t) => {
+            return <ShowTimer key={t.id} timer={t} />;
+          })}
+        <EditTimer timerListSize={timers.length} />
+      </Box>
     </>
   );
 };
