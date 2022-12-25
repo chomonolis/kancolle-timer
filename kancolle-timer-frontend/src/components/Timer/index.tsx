d26506a36f2e88baacfabca03ee88c1d39a31803
chomonolis <tyomo3317@gmail.com>
2022-12-25 18:42:04 +0900

    timersArrayとMapObjectの追加

--- kancolle-timer-frontend/src/components/Timer/index.tsx
+++ kancolle-timer-frontend/src/components/Timer/index.tsx
@@ -4,15 +4,15 @@ import ShowTimer from './ShowTimer';
 import useTimerIndex from './useIndexTimer';
 
 const Timer = () => {
-  const { timers, organizeAfterDelete } = useTimerIndex();
+  const { timersArray, organizeAfterDelete } = useTimerIndex();
   return (
     <>
       <Box sx={{ minWidth: '450px' }}>
-        {timers &&
-          timers.map((t) => {
+        {timersArray &&
+          timersArray.map((t) => {
             return <ShowTimer key={t.id} timer={t} organizeAfterDelete={organizeAfterDelete} />;
           })}
-        <EditTimer timerListSize={timers.length} />
+        <EditTimer timerListSize={timersArray.length} />
       </Box>
     </>
   );
--- kancolle-timer-frontend/src/components/Timer/useIndexTimer.ts
+++ kancolle-timer-frontend/src/components/Timer/useIndexTimer.ts
@@ -7,8 +7,19 @@ import useTimers from '../../hook/timer.hook';
 import { onCreateTimer, onDeleteTimer, onUpdateTimer } from '../../graphql/subscriptions';
 
 const useTimerIndex = () => {
-  const [timers, setTimers] = useState<Timer[]>([]);
+  const [timersArray, setTimersArray] = useState<Timer[]>([]);
+  const [timers, setTimers] = useState<Map<string, Timer>>(new Map());
   const { listTimers, updateTimer } = useTimers();
+
+  const makeTimersArray = (timers: Map<string, Timer>) => {
+    const arr = Array.from(timers.values());
+    arr.sort((a, b) => {
+      if (a.order < b.order) return -1;
+      if (a.order > b.order) return 1;
+      return 0;
+    });
+    setTimersArray(arr);
+  };
 
   const callSetTimer = useCallback(async () => {
     try {
@@ -20,7 +31,7 @@ const useTimerIndex = () => {
           if (a.order > b.order) return 1;
           return 0;
         });
-        setTimers(arr);
+        setTimersArray(arr);
       }
     } catch (e) {
       console.error(e);
@@ -29,7 +40,7 @@ const useTimerIndex = () => {
 
   const organizeAfterDelete = useCallback(
     async (deletedOrder: number) => {
-      const arr = timers.filter((t) => t.order > deletedOrder);
+      const arr = timersArray.filter((t) => t.order > deletedOrder);
       try {
         const promises = arr.map(async (t) => {
           const nextTimer = { id: t.id, order: t.order - 1 };
@@ -40,12 +51,27 @@ const useTimerIndex = () => {
         console.error(e);
       }
     },
-    [timers, updateTimer]
+    [timersArray, updateTimer]
   );
 
   useEffect(() => {
-    void callSetTimer();
-  }, [callSetTimer]);
+    void (async () => {
+      try {
+        const r = await listTimers();
+        if (r) {
+          const arr = r.filter((t) => t !== null) as Timer[];
+          const timersMap = new Map<string, Timer>();
+          for (const t of arr) {
+            timersMap.set(t.id, t);
+          }
+          setTimers(timersMap);
+          makeTimersArray(timersMap);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, [listTimers]);
 
   useEffect(() => {
     (() => {
@@ -92,7 +118,7 @@ const useTimerIndex = () => {
     })();
   }, [callSetTimer]);
 
-  return { timers, organizeAfterDelete };
+  return { timersArray, organizeAfterDelete };
 };
 
 export default useTimerIndex;
